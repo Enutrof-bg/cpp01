1837fdd7c14ab8865ca2f27c39d1989835f308b8
Kevin Wang <kevwang@f6r9s9.clusters.42paris.fr>
2025-11-12 17:33:27 +0100

    up

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -11,15 +11,31 @@ int main()
 	data->name = "bob";
 
 	tmp = Serializer::serialize(data);
+	std::cout << tmp << std::endl;
 	after = Serializer::deserialize(tmp);
+	std::cout << after << std::endl;
 
 	if (data == after)
 	{
 		std::cout << "Identique" << std::endl;
 	}
+	else
+	{
+		std::cout << "Non Identique" << std::endl;
+	}
 	
-	std::cout << after->name << std::endl;
-	std::cout << after->age << std::endl;
+	std::cout << "Before: " << std::endl;
+	std::cout << "Name: "<<after->name << " Age: "<< after->age << std::endl;
+
+	std::cout << "After: " << std::endl;
+	data->name = "bobby";
+	data->age = 24;
+	std::cout << "Name: "<<after->name << " Age: "<< after->age << std::endl;
+
+	uintptr_t test = Serializer::serialize(data);
+	Data *str = Serializer::deserialize(test);
+	(void)str;
+
 	delete data;
 
 
