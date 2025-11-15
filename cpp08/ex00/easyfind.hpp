e2def2185fea40d1a31261ee12bbd12f70593d09
Kevin Wang <kevwang@f6r9s14.clusters.42paris.fr>
2025-11-15 13:47:04 +0100

    up cpp08 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -3,17 +3,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
+#include <deque>
+#include <set>
 #include <algorithm>
 
 template <typename T>
-int easyfind(T container, int val)
+int easyfind(const T &container, int val)
 {
-	int i = 0;
-	while (container[i])
-	{
-		if (val == container[i])
-			return (i);
-		i++;
-	}
-	return (-1);
+	typename T::const_iterator it;
+
+	it = std::find(container.begin(), container.end(), val);
+	if (it == container.end())
+		throw std::runtime_error("No occurrence found");
+	return (std::distance(container.begin(), it));
 }
+
+// template <typename T>
+// int easyfind(T container, int val)
+// {
+// 	int i = 0;
+// 	while (container[i])
+// 	{
+// 		if (val == container[i])
+// 			return (i);
+// 		i++;
+// 	}
+// 	return (-1);
+// }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	std::cout << "Test1" << std::endl;
 	std::vector<int> num;
 	num.push_back(10);
 	num.push_back(20);
@@ -10,12 +11,115 @@ int main()
 	num.push_back(50);
 	for (int i = 0; i < 5; i++)
 		std::cout << num[i] << std::endl;
-	std::cout << easyfind(num, 5) << std::endl;
-	std::cout << easyfind(num, 30) << std::endl;
+	try
+	{
+		std::cout << easyfind(num, 5) << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
+	try
+	{
+		std::cout << easyfind(num, 30) << std::endl;
+		std::cout << "Found" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << "\nTest2" << std::endl;
+	std::list<int> lst;
+	lst.push_back(10);
+	lst.push_back(20);
+	lst.push_back(30);
+	lst.push_back(40);
+	lst.push_back(50);
+	 for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+        std::cout << *it << " " << std::endl;
+	try
+	{
+		std::cout << easyfind(lst, 5) << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << easyfind(lst, 30) << std::endl;
+		std::cout << "Found" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	std::cout << "\nTest3" << std::endl;
+	std::deque<int> deque;
+	deque.push_back(10);
+	deque.push_back(20);
+	deque.push_back(30);
+	deque.push_back(40);
+	deque.push_back(50);
+	deque.push_front(30);
+	for (int i = 0; i < 5; i++)
+		std::cout << deque[i] << std::endl;
+	try
+	{
+		std::cout << easyfind(deque, 5) << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << easyfind(deque, 30) << std::endl;
+		std::cout << "Found" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	std::cout << "\nTest4" << std::endl;
+	std::set<int> set;
+	set.insert(10);
+	set.insert(20);
+	set.insert(30);
+	set.insert(40);
+	set.insert(50);
+	set.insert(30);
+	for (std::set<int>::iterator it = set.begin(); it != set.end(); it++)
+		std::cout << *it << std::endl;
+	try
+	{
+		std::cout << easyfind(set, 5) << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << easyfind(set, 30) << std::endl;
+		std::cout << "Found" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	// forward_list<int> arr;
-	// arr.push_back(10);
+	// arr.insert(10);
 	// arr.push_back(20);
 	// arr.push_back(30);
 	// arr.push_back(40);
