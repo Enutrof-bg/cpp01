56a98d5c0032412b2d92d881e144bdcad79838bd
Kevin Wang <kevwang@f6r10s10.clusters.42paris.fr>
2025-11-18 19:31:10 +0100

    up

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -36,3 +36,31 @@ int easyfind(const T &container, int val)
 // typedef int caca;
 
 // caca x = 10;
+
+
+// template <typename S>
+// void swap(S &x, S &y)
+// {
+// 	S temp;
+
+// 	temp = x;
+// 	x = y;
+// 	y = temp;
+// }
+
+// template <typename T>
+// const T& min(const T &x, const T &y)
+// {
+// 	if (x == y)
+// 		return y;
+// 	return (x > y ? y : x);
+// }
+
+
+// template <typename T>
+// const T& max(const T &x, const T &y)
+// {
+// 	if (x == y)
+// 		return y;
+// 	return (x > y ? x : y);
+// }
