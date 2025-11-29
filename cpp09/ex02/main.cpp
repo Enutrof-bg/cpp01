7159f0ee2755e5a4df5dedb2663a23028a7416ac
Kevin Wang <kevwang@f6r9s10.clusters.42paris.fr>
2025-11-29 19:25:18 +0100

    up general

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -39,46 +39,50 @@ int main(int argc, char **argv)
 		std::cout << "Error: wrong arg, please positive int only 💩" << std::endl;
 		return (1);
 	}
-{
+
 	std::clock_t start;
 	double duration_vector;
 
 	start = std::clock();
 	
-	PmergeMeVector v1(argv);
+	PmergeMeVector vec1(argv);
 	std::cout << "Before:";
-	// v1.ft_print(v1.getArr());
+	// vec1.ft_print(vec1.getArr());
 	std::cout << std::endl;
 
-	std::vector<int> result = v1.ft_merge(v1.getArr());
+	std::vector<int> result_vector = vec1.ft_merge(vec1.getArr());
 
 	std::cout << "After:";
-	// v1.ft_print(v1.getArr());
+	// vec1.ft_print(vec1.getArr());
 	std::cout << std::endl;
 
 	duration_vector = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-	std::cout << std::fixed << "Time vector:" <<duration_vector << std::endl;
-}
+	// std::cout << std::fixed << "Time vector:" <<duration_vector << std::endl;
 
-{
+
+
 	std::clock_t start_deque;
 	double duration_deque;
 
 	start_deque = std::clock();
 	
-	PmergeMeDeque v1(argv);
+	PmergeMeDeque deq1(argv);
 	std::cout << "Before:";
-	// v1.ft_print(v1.getArr());
+	// deq1.ft_print(deq1.getArr());
 	std::cout << std::endl;
 
-	std::deque<int> result = v1.ft_merge(v1.getArr());
+	std::deque<int> result_deque = deq1.ft_merge(deq1.getArr());
 
 	std::cout << "After:";
-	// v1.ft_print(v1.getArr());
+	// deq1.ft_print(deq1.getArr());
 	std::cout << std::endl;
 
 	duration_deque = (std::clock() - start_deque) / (double)CLOCKS_PER_SEC;
+
+
+
+	std::cout << std::fixed << "Time vector:" <<duration_vector << std::endl;
 	std::cout << std::fixed <<"Time deque:" << duration_deque << std::endl;
-}
+
 	return (0);
 }
